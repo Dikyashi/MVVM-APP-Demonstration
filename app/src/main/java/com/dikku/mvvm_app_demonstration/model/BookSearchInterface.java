80f95ad83276bc095d04010913c9364abfae172a
DikyashiTamang <dikyashitamang@gmail.com>
2022-02-25 23:39:32 +0530

    "Added Retrofit Interface for network calls"

---
+++ app/src/main/java/com/dikku/mvvm_app_demonstration/model/BookSearchInterface.java
@@ -0,0 +1,16 @@
+package com.dikku.mvvm_app_demonstration.model;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+/**
+ * Created by dikyashitamang on 25/02/22
+ */
+public interface BookSearchInterface {
+
+    @GET("/books")
+    Call<List<VolumeResponse>> searchBook(@Query("q") String key,@Query("inauthor") String author);
+}
